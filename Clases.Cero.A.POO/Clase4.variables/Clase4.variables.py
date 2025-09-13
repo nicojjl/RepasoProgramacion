ecc842db88dbc3422d0c99e1ec47c0ddae516991
nico <nsilvad@usm.cl>
2025-09-14 01:08:00 +0200

    Agrega Clase4.variables.py

---
+++ Clases.Cero.A.POO/Clase4.variables/Clase4.variables.py
@@ -0,0 +1,46 @@
+# Variables numericas
+numeroEntero = 10
+numeroReal = 5.5
+
+#Variables de texto
+
+nombre = "jose"
+apellido = 'perez'
+
+#variables booleanas
+
+cierto = True #Tambien puede ser 1
+falso = False #Tambien puede ser 0
+
+#Aritmeticos
+
+suma = 5 + 3
+#resultado: 8
+resta = 5 - 3
+#resultado: 2
+multiplicacion = 5 * 3
+#resultado: 15
+division = 5 / 3 #Division normal 
+#resultado: 1.6666666666666667
+divisionEntera = 5 // 3 #Division entera
+#resultado: 1
+modulo = 5 % 3 #Resto de la division
+#resultado: 2
+exponente = 5 ** 3 #Potencia
+#resultado: 125
+
+#Relacionales
+
+comparacion = 5 == 5 #Igualdad
+diferente = 5 != 5 #Diferente
+mayorQue = 5 > 3 #Mayor que
+menorQue = 5 < 3 #Menor que
+mayorIgual = 5 >= 5 #Mayor o igual que
+menorIgual = 5 <= 3 #Menor o igual que
+
+#Logicos
+
+logicoand = True and False 
+logicoOr = True or False
+logicoNot = not True
+logicoMix = (5 > 3) and (3 < 1) or not(False)
