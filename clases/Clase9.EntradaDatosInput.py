9a3eafd35c3e59c188612b481acc5b2034246bf4
nico <nsilvad@usm.cl>
2025-09-15 19:15:56 +0200

    Agrega Clase9.EntradaDatosInput.py

---
+++ clases/Clase9.EntradaDatosInput.py
@@ -0,0 +1,29 @@
+#Entrada de datos del tipo string o cadena
+'''
+nombre = input("ingresa tu nombre: ")
+
+
+print(nombre)
+'''
+
+#Entrada de datos del tipo entero
+'''
+resultado = 0
+a = 0
+b = 3
+
+a = int(input("ingresa un numero: "))
+
+resultado = a + b
+print("el resultado es:", resultado)
+'''
+
+
+#Entrada de datos del tipo flotante
+a = float(input("ingresa un numero con decimales: "))
+b=5.5
+resultado = a + b
+#concatenacion
+print("el resultado es:" + str(resultado))
+#interpolacion
+print(f"el resultado 2 es: {resultado}")
